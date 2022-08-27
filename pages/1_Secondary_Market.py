9ff2f3f1297c960eaca1f678888fd4d7741d504a
Ryan Anderson <98444459+Ryanderson94@users.noreply.github.com>
2022-08-27 10:26:32 -0400

    Update 1_Secondary_Market.py

--- pages/1_Secondary_Market.py
+++ pages/1_Secondary_Market.py
@@ -17,8 +17,6 @@ w3_wallet = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
 
 from pinata import pin_file_to_ipfs, pin_json_to_ipfs, convert_data_to_json
 
-from functions import get_location, get_hotels
-
 load_dotenv()
 
 # Create an instance of web3.py for communicationn to the Blockchain smart contract
@@ -65,12 +63,11 @@ def pin_historical_price_report(report_content):
 
 ################################################################################
 
-
 from crypto_wallet import generate_account, get_balance, send_transaction
 
 ################################################################################
 
-## Below are database stored with fake/stock hotel infor & images for website display purpose (not usefule real codes)
+## Below are database stored with fake/stock hotel infor & images for website display purposs
 hotel_database = {
     "1 Hotel Toronto": ["1 Hotel Toronto", "0x05d38543486F918D1d0fFB73E074e90445dD9E5D", "4.3", .20, "Images/1 Hotel Toronto.jpeg"],
     "The Omni King Edward Hotel": ["The Omni King Edward Hotel", "0x2422858F9C4480c2724A309D58Ffd7Ac8bF65396", "5.0", .33, "Images/The Omni King Edward Hotel.jpeg"],
@@ -120,9 +117,7 @@ tokens_on_secondary_market_list = list(df_hotels_on_secondary_market_list)
 ################################################################################
 # Streamlit Sidebar Code - Start
 st.sidebar.markdown("## USD/Ether Converter")
-
-
-st.sidebar.markdown("## Current Account Address and Ether Balance in Ether")
+st.sidebar.markdown("## Current Account Address and Ether Balance")
 
 ##########################################
 
@@ -145,27 +140,22 @@ st.sidebar.markdown("## SELL")
 ## show existing token list
 
 tokens = contract.functions.totalSupply().call()
-token_id_listed = st.sidebar.selectbox("List Your Hotel Token ID to sell", list(range(tokens)))
+token_id_listed = st.sidebar.selectbox("Select a Reservation to Sell", list(range(tokens)))
 
 booking_info_listed = contract.functions.roomconfirmation(token_id_listed).call()
 
-## st.sidebar.text(booking_info_listed)
 st.sidebar.write("Hotel Name: ", booking_info_listed[0])
-st.sidebar.write("Start Date: ",booking_info_listed[1])
-st.sidebar.write("End Date: ",booking_info_listed[2])
-## st.sidebar.write("Confirmation Note: ",booking_info_listed[3])
-st.sidebar.write("Price purchased: ",booking_info_listed[4])
+st.sidebar.write("Check In: ",booking_info_listed[1])
+st.sidebar.write("Check Out: ",booking_info_listed[2])
+st.sidebar.write("Purchase Price: ",booking_info_listed[4])
 
-price_list_for_sale = st.sidebar.number_input("price listed for sale")
-st.sidebar.write("price_list_for_sale: ",price_list_for_sale)
+price_list_for_sale = st.sidebar.number_input("Sale Price (ETH)")
 
 # Identify the seller Ethereum Address
-#seller_address = "0x05d38543486F918D1d0fFB73E074e90445dD9E5D"
-seller_address = st.sidebar.text_input("Input seller Address")
-st.sidebar.write("seller_address", seller_address)
+seller_address = st.sidebar.text_input("Input Wallet Address")
 
 ## below part enable seller to add their token to secondary market database
-if st.sidebar.button("List token on Sale"):
+if st.sidebar.button("Finalize Token Sale"):
     st.sidebar.write("token_id_listed",token_id_listed)
     st.sidebar.write("booking_info_listed",booking_info_listed)
     ## add other info to booking info like: listed price, token id, seller's address to receive proceeds from the sale
@@ -193,38 +183,28 @@ st.sidebar.markdown("## BUY")
 
 tokens = contract.functions.totalSupply().call()
 
-token_id_listed = st.sidebar.selectbox("Choose a token to buy", list(range(tokens)))
-
-
-## st.sidebar.markdown("## Hotel Name, Daily Rate, and Ethereum Address")
-st.sidebar.write("Token selected to buy ", token_id_listed)
+token_id_listed = st.sidebar.selectbox("Select Reservation to Purchase", list(range(tokens)))
 
 seller_address = df_hotels_on_secondary_market_list.loc[str(token_id_listed),'seller address']
-st.sidebar.write("seller_address", seller_address)
+st.sidebar.write("Seller Address", seller_address)
 
 
 buyer_address = account.address
-st.sidebar.write("buyer_address", buyer_address)
+st.sidebar.write("Buyer Address", buyer_address)
 
-st.sidebar.markdown("buyer's Ethereum Balance")
+st.sidebar.markdown("Ethereum Balance Available")
 
 # Write the inTech Finder candidate's Ethereum balance to the sidebar
 st.sidebar.write(get_balance(w3_wallet,account.address)) 
 
 
 # Write the inTech Finder candidate's Daily rate to the sidebar
-price_to_pay_seller = st.sidebar.number_input("price_to_pay_seller")
-st.sidebar.write("price_to_pay_seller: ",price_to_pay_seller)
-
-
-st.sidebar.markdown("## Total Proceeds (ether) to be paid")
-
-st.sidebar.write(price_to_pay_seller)
+price_to_pay_seller = st.sidebar.number_input("Purchase Price")
 
 
 ##########################################
 
-if st.sidebar.button("Pay seller & Transfer NFT Ownership"):
+if st.sidebar.button("Pay Seller & Transfer NFT Ownership"):
     # Transfer NYT
     contract.functions.transferFrom(seller_address,buyer_address,token_id_listed).transact({"from": seller_address, "gas": 3000000})
     st.sidebar.write("#### Just transfered ownership of token ID: ",token_id_listed)
